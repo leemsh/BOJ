a2962c88fc87a94ad4c9ee7264fbce431324f688
Luke <g27776804@gmail.com>
2024-11-05 17:39:09 +0900

    BOJ 9465 temp

---
+++ 9465.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t, n;cin>>t>>n;
+    while(t--) {
+
+    }
+    return 0;
+}
